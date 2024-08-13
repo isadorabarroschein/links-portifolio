0a895bee4ce5c57232ff0e7185345b244c6a8ad0
David <davidasafe07@gmail.com>
2024-08-13 10:11:55 -0400

    fix

--- script.js
+++ script.js
@@ -8,11 +8,8 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png");
-    img.setAttribute(
-      "alt",
-      "Foto de Wanderson Timóteo, usando óculos escuro e camiseta salmão, barba e fundo gradiente com as cores roxo escuro e roxo claro."
-    );
+    img.setAttribute("src", "./assets/avatar-light.jpg");
+    img.setAttribute("alt", "Princesinha Sophia");
   } else {
     // set tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png");
